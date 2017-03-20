ae597f0dff6f9dc23e29f892b384abd87d9bb74a
utshar <utshar@microsoft.com>
2017-03-20 18:48:51 +0530

    adding the controller

---
+++ AzureBillingAnalytics/BillingDataApi/Controllers/CspUtilizationController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BillingDataApi.Controllers
+{
+    [RoutePrefix("api/csputilization")]
+    public class CspUtilizationController : ApiController
+    {
+        [Route(@"")]
+        public string GetAllData()
+        {
+            return "Here is the controller default method";
+        }
+    }
+}
